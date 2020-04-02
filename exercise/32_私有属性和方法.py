53f84707864acf7bb56d05a44db8d64b31c79385
GuangGuangLi <15607521232@163.com>
2020-04-02 16:06:54 +0800

    私有化

---
+++ exercise/32_私有属性和方法.py
@@ -0,0 +1,33 @@
+#coding=utf-8
+
+class Person():
+    def __init__(self,name,age,hobby):
+        self.name = name
+        self._age = age
+        self.__hobby = hobby
+
+    def show_person(self):
+        print(self.name)
+        print(self._age)
+        print(self.__hobby)
+
+class Student(Person):
+    def construction(self,name,age,hobby):
+        self.name = name
+        self._age = age
+        self.__hobby = hobby
+    def show_student(self):
+        print(self.name)
+        print(self._age)
+        print(self.__hobby)
+
+s1 = Student('su',25,'baseball')
+# s1.show_student()
+s1.show_person()
+print("*" * 20)
+s1.construction('li',30,'cook')
+s1.show_person()
+print("*" * 20)
+s1.show_student()
+
+
