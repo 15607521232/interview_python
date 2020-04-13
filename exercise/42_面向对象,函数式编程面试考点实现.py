ef92cbaa5d498744a48a71cd81ab59dad21bf1d9
GuangGuangLi <15607521232@163.com>
2020-04-13 14:32:41 +0800

    exer

--- exercise/42_面向对象,函数式编程面试考点实现.py
+++ exercise/42_面向对象,函数式编程面试考点实现.py
@@ -2,7 +2,12 @@
 
 #用函数方法实现计算集合 list1 = [1,2,3,4,5]中，所有元素的和
 
-# def list_add(list):
-# #     for i in list:
-# #         while i in list:
-# #             sum = i +
+list1 = [1,2,3,4,5]
+def list_sum(list):
+    x = 0
+    for i in list1:
+        x += i
+    return x
+
+li_sum = list_sum(list1)
+print(li_sum)
